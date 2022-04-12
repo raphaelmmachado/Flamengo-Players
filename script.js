081152be80d2c7486ecdc879f06205fe8ee38b74
Raphael Machado <raphael.mm.91@gmail.com>
2022-04-12 13:11:16 -0300

    removed search by ability

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const getAllPlayers = async () => {
   const url = "http://localhost:8000";
   const response = await fetch(`${url}/get`);
   const data = await response.json();
-  
-  const allPlayers = data.reduce((accumulator, player) => {
+
+  const sortByAbility = data.sort((a, b) => b.ability - a.ability);
+
+  const allPlayers = sortByAbility.reduce((accumulator, player) => {
     accumulator += `
       <div class="card">
         <img class="photo" src=${player.img}
@@ -48,14 +50,15 @@ const getAllPlayers = async () => {
   const wrapper = document.querySelector("[data-wrapper]");
   wrapper.innerHTML = allPlayers;
 };
-document.addEventListener("DOMContentLoaded", getAllPlayers);
+document.addEventListener("DOMContentLoaded", getAllPlayers());
 
 const getPlayerByName = async (input) => {
   const url = "http://localhost:8000";
   const response = await fetch(`${url}/get`);
   const data = await response.json();
 
-  const filterPlayersByName = data.filter((player) => {
+  const sortByAbility = data.sort((a, b) => b.ability - a.ability);
+  const filterPlayersByName = sortByAbility.filter((player) => {
     const name = player.name.toLowerCase();
     const known = player.alsoKnownAs ?? name;
     return name.includes(input) || known.toLowerCase().includes(input)
@@ -102,61 +105,4 @@ const getPlayerByName = async (input) => {
 searchBar.addEventListener("input", (e) => {
   const input = e.target.value.toLowerCase();
   return getPlayerByName(input);
-});
-
-const getPlayerByAbility = async (inputValue) => {
-  const url = "http://localhost:8000";
-  const response = await fetch(`${url}/get`);
-  const data = await response.json();
-
-  const sortByAbility = data.sort((a, b) => b.ability - a.ability);
-
-  const filterPlayersByAbility = sortByAbility.filter((player) => {
-    const inputToNumber = parseInt(inputValue);
-    const abilityToNumber = player.ability;
-    return abilityToNumber <= inputToNumber;
-  });
-  const playersByAbility = filterPlayersByAbility
-    .map((player) => {
-      return (player = `
-    <div class="card">
-      <img class="photo" src=${player.img}
-      alt="football player">
-      <div class="profile1">
-      <div class="name">
-          <img src='./images/fla.png' class='logo'>
-           <div>${player.name}</div>
-          <img class='country' src='${player.country}'></div>
-        <div class="profile2">
-          <div class="profile3">
-          <div>Idade:</div>
-          <div>${player.age}</div>
-          </div>
-          <div class="profile3">
-          <div>Camisa:</div>
-          <div>${player.number}</div>
-          </div>
-          <div class="profile3">
-          <div>Posição:</div>
-          <div>${player.position}</div>
-          </div>
-          <div class="profile3">
-          <div>Habilidade:</div>
-          <div>${player.ability}</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    `);
-    })
-    .join("");
-
-  const wrapper = document.querySelector("[data-wrapper]");
-  wrapper.innerHTML = playersByAbility;
-};
-
-abilityInput.addEventListener("input", (e) => {
-  const inputValue = e.target.value;
-  if (inputValue === '') return getAllPlayers()
-  return getPlayerByAbility(inputValue);
 });
